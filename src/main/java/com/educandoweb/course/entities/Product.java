6c55a0476dd6ea46820e72b5d46ef12489f26fd0
Edmir Cardoso <edmircrds@gmail.com>
2025-07-01 16:56:51 -0100

    OrderItem, many-to-many association with extra attributes PART1

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -3,9 +3,6 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,7 +26,6 @@ public class Product implements Serializable {
 	private String imgUrl;
 	
 	@ManyToMany
-	@JsonIgnore
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id")
 			,inverseJoinColumns = @JoinColumn(name ="category_id") )
 	private Set<Category> categories = new HashSet<>();
